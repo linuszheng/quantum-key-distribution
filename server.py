e7ce6fee3e2d77573b93480c43b4d41a5fa941cf
Linus Zheng <linus.zheng@warriorlife.net>
2021-10-24 03:59:11 -0500

    uh quantum is weird

--- server.py
+++ server.py
@@ -3,44 +3,17 @@ from qiskit import QuantumCircuit, assemble, Aer
 from random import randint
 
 app = Flask(__name__)
+sim = Aer.get_backend('aer_simulator')
 
-qc=0
 
-def getResults():
-	sim = Aer.get_backend('aer_simulator') 
-	result = sim.run(qc).result()
+circuits = []
+qubits = []
+
+#get count results of circuit q
+def getCounts(q):
+	result = sim.run(q).result()
 	counts = result.get_counts()
 	return counts
-
-def measureStandard(bit):
-	qc.measure(bit,bit)		# outputs measurement of qbit0 to cbit0
-
-def measureHadamard(bit):
-	qc.h(0)
-	qc.measure(bit,bit)
-
-def createCircuit(size):
-	qc = QuantumCircuit(size,size)	# 2 quantum bits, 2 classical bits
-	return qc
-
-def generateRandom(size):
-	for i in range(size):
-		x = randint(2)
-		y = randint(2)
-		if x==1:
-			qc.x(i)
-		if y==1:
-			qc.h(i)
-
-def initializeCircuit(size):
-	qc = createCircuit(int(size))
-	qc.h(0)
-	qc.h(1)
-	measureStandard(0)
-	measureHadamard(1)
-	measureStandard(2)
-	measureHadamard(3)
-	return getResults(qc)
 	
 #classical binary bitstring length n
 def randomBitstring(n):
@@ -67,17 +40,19 @@ def qubitString(vector):
 
 # APIS to implement
 
-sim = Aer.get_backend('aer_simulator')
 
 # input: n
 # return a, b as strings, and qubits are an array of strings
 # generates a, b as random bit strings of length n, from which it generates the qubits
+@app.route('/generateAlice/<int:n>')
 def generateAlice(n):
+	# global qubits
 	a = randomBitstring(n)
 	b = randomBitstring(n)
 	qubits = [""]*n
 	for i in range(n):
-		q = createCircuit(1)
+		q = QuantumCircuit(1,1)
+		circuits.append(q)
 		if(int(a[i])==1):
 			q.x(0)
 		if(int(b[i])==1):
@@ -87,24 +62,60 @@ def generateAlice(n):
 		result = sim.run(q).result()
 		vector = result.get_statevector()
 		qubits[i] = qubitString(vector)
-	return[a,b,qubits]
+	# print(a)
+	# print(b)
+	# print(qubits)
+	return {
+		"a": a,
+		"b": b,
+		"qubits": qubits
+	}
 	
 # index: the qubit to measure, basis: 0 for 0, 1, 1 for + , -
 # measures the qubit, returns the result as a string. 
+@app.route('/measureQubit/<int:index>/<int:basis>')
 def measureQubit(index, basis):
-    pass
+	q = circuits[index]
+	result = sim.run(q).result()
+	vector = result.get_statevector()
+	# print(vector)
+	# print(index)
+	# print(qubits)
+	# print(qubits[index])
+	# print(qubitString(vector))
+	if basis==1:
+		q.h(0)
+	q.measure(0,0)
+	if basis==1:
+		q.h(0)
+	results = getCounts(q)
+	# print(results)
+	res="1"
+	if len(results.keys())>1:
+		res=list(results.keys())[randint(0,1)]
+	elif list(results.keys())[0]=="0":
+		res="0"
+	if basis==0:
+		return res
+	else:
+		return "+" if res=="1" else "-"
 
 # first randomly generate b', then measures all the qubits.
 # return b' as string, measurement outcome as array of strings.
-def measureBob(): 
-    pass
+def measureBob(n): 
+	b2 = randomBitstring(n)
+	result=[""]*n
+	for i in range(n):
+		q = circuits[i]
+		result[i]=getCounts(q)[0]
+	return (b2, result)
 
 
 def sendBases(isBob):
-    pass
+	pass
 
 # drop the list of indices from the state
 def drop(indices):
-    pass
+	pass
 
 
