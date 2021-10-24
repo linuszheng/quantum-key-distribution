af3a375ae02f266ce8ab1c74ad7883031e8624d0
Letizia Fazzini <letizia.fazzini.tx@gmail.com>
2021-10-24 02:28:03 -0500

    implemented generateAlice

--- server.py
+++ server.py
@@ -41,15 +41,54 @@ def initializeCircuit(size):
 	measureStandard(2)
 	measureHadamard(3)
 	return getResults(qc)
+	
+#classical binary bitstring length n
+def randomBitstring(n):
+	bitstring = ""
+	for i in range(n):
+		x = randint(1,2)
+		bitstring += "0" if x==1 else "1"
+	return bitstring
+	
+#assigns string value (0,1,+,-) to qubit
+def qubitString(vector):
+	x = vector[0].real
+	y = vector[1].real
+	if x==1 and y==0:
+		return "0"
+	elif x==0 and y==1:
+		return "1"
+	elif y>0:
+		return "+"
+	elif y<0:
+		return "-"
+	return
+
 
 # APIS to implement
+
+sim = Aer.get_backend('aer_simulator')
 
 # input: n
 # return a, b as strings, and qubits are an array of strings
 # generates a, b as random bit strings of length n, from which it generates the qubits
 def generateAlice(n):
-    pass
-
+	a = randomBitstring(n)
+	b = randomBitstring(n)
+	qubits = [""]*n
+	for i in range(n):
+		q = createCircuit(1)
+		if(int(a[i])==1):
+			q.x(0)
+		if(int(b[i])==1):
+			q.h(0)
+		#from measurment tutorial
+		q.save_statevector()
+		result = sim.run(q).result()
+		vector = result.get_statevector()
+		qubits[i] = qubitString(vector)
+	return[a,b,qubits]
+	
 # index: the qubit to measure, basis: 0 for 0, 1, 1 for + , -
 # measures the qubit, returns the result as a string. 
 def measureQubit(index, basis):
