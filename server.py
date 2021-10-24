7a73cb9732c10acbc1a3dfc2cb0360c8fc85b295
Linus Zheng <linus.zheng@warriorlife.net>
2021-10-23 19:09:14 -0500

    generaterandom

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
 from flask import Flask
 from qiskit import QuantumCircuit, assemble, Aer
+from random import randint
 
 app = Flask(__name__)
 
 qc=0
 
-def getResults(qc):
+def getResults():
 	sim = Aer.get_backend('aer_simulator') 
 	result = sim.run(qc).result()
 	counts = result.get_counts()
 	return counts
 
-def measureStandard(qc, bit):
+def measureStandard(bit):
 	qc.measure(bit,bit)		# outputs measurement of qbit0 to cbit0
 
-def measureHadamard(qc, bit):
+def measureHadamard(bit):
 	qc.h(0)
 	qc.measure(bit,bit)
 
@@ -22,15 +23,24 @@ def createCircuit(size):
 	qc = QuantumCircuit(size,size)	# 2 quantum bits, 2 classical bits
 	return qc
 
+def generateRandom(size):
+	for i in range(size):
+		x = randint(2)
+		y = randint(2)
+		if x==1:
+			qc.x(i)
+		if y==1:
+			qc.h(i)
+
 @app.route("/<size>")
 def initializeCircuit(size):
 	qc = createCircuit(int(size))
 	qc.h(0)
 	qc.h(1)
-	measureStandard(qc, 0)
-	measureHadamard(qc, 1)
-	measureStandard(qc, 2)
-	measureHadamard(qc, 3)
+	measureStandard(0)
+	measureHadamard(1)
+	measureStandard(2)
+	measureHadamard(3)
 	return getResults(qc)
 
 
