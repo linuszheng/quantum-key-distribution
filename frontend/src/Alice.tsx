b6b05d6b6c409214f604d0be3a5b70057e3dbbe2
Linus Zheng <linus.zheng@warriorlife.net>
2021-10-24 19:46:59 -0500

    a

--- frontend/src/Alice.tsx
+++ frontend/src/Alice.tsx
@@ -12,19 +12,32 @@ const Alice = () => {
       console.log("Websocket connected: " + socket.connected);
     });
 
-    socket.on("custom-server-msg", (data: any) => {
-      console.log("Data received: " + data.data);
+    socket.on("qubitsGenerated", (data: any) => {
+      console.log("a: " + data.a);
+      console.log("b: " + data.b);
+      console.log("qubits: " + data.qubits);
     });
+
+    socket.on("qubitMeasured", (data: any) => {
+      console.log("eve measured: " + data);
+    });
+
+    socket.on("qubitsMeasured", (data: any) => {
+      console.log("bob's bases: "+data.b2);
+      console.log("bob measured: " + data.result);
+    });
+
   };
   return (
     <div>
       Alice
       <button
         onClick={() => {
-          // @ts-ignore
-          window.socket.emit("messsage", "hey");
+          socket.emit("generateAlice", 5);
+          socket.emit("measureEve", 2, 0);
+          socket.emit("measureBob", 5);
         }}
-      ></button>
+      >Generate Alice Qubits</button>
     </div>
   );
 };
