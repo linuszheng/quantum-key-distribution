ddbe8a3408a3b3f8429b475f1b7cb745d9addc5f
Linus Zheng <linus.zheng@warriorlife.net>
2021-10-24 20:21:09 -0500

    bob changed :o

--- frontend/src/Bob.tsx
+++ frontend/src/Bob.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Bob = () => {
+
   // @ts-ignore
   const socket = window.socket;
   const setSocketListeners = () => {
@@ -9,12 +10,31 @@ const Bob = () => {
     });
 
     socket.on("qubitsGenerated", (data: any) => {
-      console.log(data)
+      // create string of unknown (question mark) qubit values
+      console.log(data.qubits);
+    });
+    socket.on("qubitsMeasured", (data: any) => {
+      // reveal qubit values
+      console.log(data.result);
+      // send b2 over classical channel??
+      console.log(data.b2);
     });
   };
 
-
-  return <div>Bob</div>;
+  return (<div>Bob
+    <button
+    onClick={() => {
+      socket.emit("measureBob", 5);
+    }}>
+      Measure Bob
+  </button>
+  <button
+    onClick={() => {
+      socket.emit("drop", 5);
+    }}>
+      Measure Bob
+  </button>
+  </div>);
 };
 
 export default Bob;
