c7f99081d60a6edf07a62870b539fcf0082ed84e
Steve Han <stevehan2001@gmail.com>
2021-10-24 23:48:50 -0500

    actual ngrok

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ import { CardActionArea } from "@mui/material";
 import { css, jsx } from "@emotion/react";
 
 const App = () => {
-  const socket = io("http://localhost:5000");
+  const socket = io("http://c4c6-128-62-34-246.ngrok.io");
   window.socket = socket;
   useEffect(() => {
     setSocketListeners();
