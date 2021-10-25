062b6b859254faeb07c1c5b4c0511b3a43ebf2ee
Steve Han <stevehan2001@gmail.com>
2021-10-24 19:47:52 -0500

    new alice

--- frontend/src/Alice.tsx
+++ frontend/src/Alice.tsx
@@ -1,11 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Card,
+  createTheme,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
+import { styled } from "@mui/system";
+import QuantumState from "./QuantumState";
 
 const Alice = () => {
   // @ts-ignore
   const socket = window.socket;
+
   useEffect(() => {
     setSocketListeners();
   }, []);
+
+  const [qubits, setQubits] = useState([
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "+",
+    "0",
+    "0",
+    "0",
+    "0",
+    "0",
+    "+",
+  ]);
 
   const setSocketListeners = () => {
     socket.on("connect", () => {
@@ -16,17 +41,71 @@ const Alice = () => {
       console.log("Data received: " + data.data);
     });
   };
+
+  const aliceTheme = createTheme({
+    palette: {
+      primary: {
+        light: "#8E05C2",
+        main: "#a58685",
+        dark: "#852747",
+      },
+      mode: "dark",
+    },
+  });
+
   return (
-    <div>
-      Alice
-      <button
-        onClick={() => {
-          // @ts-ignore
-          window.socket.emit("message", "hey");
+    <ThemeProvider theme={aliceTheme}>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100vh",
+          bgcolor: "#F5C6A5",
+          padding: "3rem",
+          boxSizing: "border-box",
         }}
-      ></button>
-    </div>
+      >
+        <Typography variant="h2" color="primary.light" fontWeight="800">
+          👧 Alice
+        </Typography>
+        <ChannelContainer>
+          <Typography variant="h6" color="text.main" fontWeight="600">
+            Quantum Channel
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "1rem",
+            }}
+          >
+            <QuantumState state={qubits} showQubit={(idx) => {}} />
+          </div>
+        </ChannelContainer>
+        <ChannelContainer>
+          <Typography variant="h6" color="text.main" fontWeight="600">
+            Classical Channel
+          </Typography>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop: "1rem",
+            }}
+          >
+            a = 10001010101010
+          </div>
+        </ChannelContainer>
+      </Box>
+    </ThemeProvider>
   );
 };
+const ChannelContainer = styled(Card)(({ theme }) => ({
+  backgroundColor: theme.palette.primary.dark,
+  margin: "5rem 3rem",
+  borderRadius: "2rem",
+  padding: "2rem",
+}));
 
 export default Alice;
